ce45d1a57a30aed630a24daeb27f740e89723071
alexavil <alexavil007@gmail.com>
2026-07-31 22:26:11 +0400

    base review submit

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const pb = require("../db")
+
+async function loadTasks() {
+    return await pb.collection('tasks').getFullList()
+}
+
+async function loadAnswers() {
+    return await pb.collection('answers').getFullList()
+}
+
+async function sendComment(body) {
+    const record = await pb.collection('comments').create(body);
+}
+
 /* GET review page. */
-router.get('/', function(req, res, next) {
-    res.render('review', { title: 'Twistask' });
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await loadTasks();
+        const answers = await loadAnswers();
+        res.render('review', { title: 'Twistask', timed: false, tasks, answers });
+    } catch (err) {
+        next(err); // lets Express error middleware handle/log and return a 500
+    }
 });
+
+router.post('/submit', async (req, res, next) => {
+    try {
+        let body = req.body;
+        const result = await sendComment(body);
+        console.log(result);
+    } catch (err) {
+        next(err); // lets Express error middleware handle/log and return a 500
+    }
+})
 
 module.exports = router;
