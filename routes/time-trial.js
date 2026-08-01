3cce221c91d2186b37c0e4ad0736f78b28ce8a9b
alexavil <alexavil007@gmail.com>
2026-08-01 21:17:28 +0400

    time-trial base

--- routes/time-trial.js
+++ routes/time-trial.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const pb = require("../db")
+
+async function loadTasks() {
+    return await pb.collection('tasks').getFullList()
+}
+
+async function sendAnswer(body) {
+    const record = await pb.collection('answers').create(body);
+}
+
 /* GET time-trial main page. */
 router.get('/', function(req, res, next) {
     res.render('time-trial', { title: 'Twistask' });
 });
-router.get('/test', function(req, res, next) {
-    res.render('challenge', { title: 'Twistask', timed: true });
+router.get('/test', async function (req, res, next) {
+    const tasks = await loadTasks();
+    console.log(tasks);
+    res.render('challenge', {title: 'Twistask', timed: true, tasks});
 });
 
 module.exports = router;
